246458116d11122af826ca6371444ce9097ec5f4
christian gomez <christian gomez>
2017-04-26 16:12:51 +0200

    Exportar proyecto

---
+++ src/prueba.java
@@ -0,0 +1,36 @@
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+
+public class prueba extends TelegramLongPollingBot {
+    @Override
+    public void onUpdateReceived(Update update) {
+    	// We check if the update has a message and the message has text
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            SendMessage message = new SendMessage() // Create a SendMessage object with mandatory fields
+                    .setChatId(update.getMessage().getChatId())
+                    .setText(update.getMessage().getText());
+            try {
+                sendMessage(message); // Call method to send the message
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        // TODO
+    	 return "MusicBotDasi_bot";
+    }
+
+    @Override
+    public String getBotToken() {
+        // TODO
+        return "350608468:AAGODvQiRTkkDlSUZ9evgqHwqmnRfIKaGy8";
+    }
+    
+    
+}
