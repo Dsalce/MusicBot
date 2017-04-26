10beb123b676534fc34c4b15ed3a2e21b5a8612e
CHRISTIAN\christian <christiangomez.r@hotmail.com>
2017-04-26 18:17:07 +0200

    añadir  comentario

--- src/main.java
+++ src/main.java
@@ -10,6 +10,7 @@ public class main {
 		public static void main(String[] args) {
 
 	        ApiContextInitializer.init();
+	        //hola
 
 	        TelegramBotsApi botsApi = new TelegramBotsApi();
 
