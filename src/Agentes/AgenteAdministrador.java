c88287865f928f878fa4a8375ba3b6038e7955ba
CHRISTIAN\christian <christiangomez.r@hotmail.com>
2017-05-03 18:33:22 +0200

    Aniadir agentes
    Se añaden los 3 agentes junto con la libreria de Jade

---
+++ src/Agentes/AgenteAdministrador.java
@@ -0,0 +1,13 @@
+package Agentes;
+import jade.core.Agent;
+
+public class AgenteAdministrador extends Agent {
+	protected void setup() {
+
+		// inicialización de MiAgente }
+	}
+
+	protected void takeDown() { // liberación de recursos del agente }
+	}
+}
+
---
+++ src/Agentes/AgenteIdentificador.java
@@ -0,0 +1,12 @@
+package Agentes;
+import jade.core.Agent;
+
+public class AgenteIdentificador extends Agent {
+	protected void setup() {
+
+		// inicialización de MiAgente }
+	}
+
+	protected void takeDown() { // liberación de recursos del agente }
+	}
+}
