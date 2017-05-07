9a857cfe9e9f74bd72db5809749c33f5d99bc18d
Dsalce <dsalce92@gmail.com>
2017-05-07 14:31:00 +0200

    Add files via upload
    utiles yreglas

---
+++ src/utiles/LlamarReglas.java
@@ -0,0 +1,31 @@
+package utiles;
+
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+
+import control.Message;
+
+public class LlamarReglas {
+
+	
+	public void  reglasIni(){
+		try {
+            // load up the knowledge base
+	        KieServices ks = KieServices.Factory.get();
+    	    KieContainer kContainer = ks.getKieClasspathContainer();
+        	KieSession kSession = kContainer.newKieSession("ksession-rules");
+
+            // go !
+            Message message = new Message();
+            message.setMessage("Hello World");
+            message.setStatus(Message.HELLO);
+            kSession.insert(message);
+            kSession.fireAllRules();
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
+	}
+	
+	
+}
